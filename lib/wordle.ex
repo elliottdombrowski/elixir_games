bea925c4fdcb213e83149f5faff507e83692607a
elliott dombrowski <elliottroyal@me.com>
2023-10-11 18:00:22 -0500

    green / grey cases

--- lib/wordle.ex
+++ lib/wordle.ex
@@ -2,34 +2,74 @@ defmodule Games.Wordle do
   def play, do: play(generate_answer(), 1)
 
   def play(answer, attempt) do
-    user_input = String.trim IO.gets "Enter a 5-letter word: "
+    user_input = String.trim(IO.gets("Enter a 5-letter word: "))
+
+    answer_list = parse_string(answer)
+    user_input = parse_string(user_input)
 
     if attempt < 5 do
-      result = calculate_guess(answer, user_input)
-      if result == [:grey, :green, :green, :green, :green] do
-        IO.puts "You win."
+      result = calculate_guess(answer_list, user_input)
+
+      if result == [:green, :green, :green, :green, :green] do
+        IO.puts("You win.")
       else
         play(answer, attempt + 1)
       end
     else
-      IO.puts """
+      IO.puts("""
         You lose!
         The correct answer was '#{answer}'
-      """
+      """)
     end
   end
 
-  def calculate_guess(answer, user_input) do
-    IO.puts answer
-    IO.puts user_input
+  def calculate_guess(answer_list, user_input) do
+    Enum.map(user_input, fn x ->
+      # if Enum.member?(answer_list, x) do
+      #   :green
+      # else
+      #   :grey
+      # end
+    end)
+  end
 
-    # input_enum = String.graphemes(user_input)
-    # answer_enum = String.graphemes(answer)
+  def check_green(answer_list, user_input) do
+    Enum.zip(answer_list, user_input)
+    |> Enum.map(fn {a, u} ->
+      if a == u do
+        :green
+      else
+        u
+      end
+    end)
+  end
 
-    [:grey, :green, :green, :green, :green]
+  def check_yellow(answer_list, user_input) do
+    Enum.map(user_input, fn u ->
+      if Enum.member?(answer_list, u) and is_bitstring u do
+        :yellow
+      else
+        u
+      end
+    end)
+  end
 
-    # IO.ANSI.yellow()
-    # IO.ANSI.light_black()
+  def check_grey(_answer_list, user_input) do
+    Enum.map(user_input, fn u ->
+      # Default any non-green / yellow items to :grey
+      if is_atom u do
+        u
+      else
+        :grey
+      end
+    end)
+  end
+
+  def parse_string(string) do
+    string
+    |> String.trim()
+    |> String.downcase()
+    |> String.split("", trim: true)
   end
 
   def generate_answer do
--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -2,9 +2,42 @@ defmodule WordleTest do
   use ExUnit.Case
 
   # Games.Wordle.play(//answer, //user_input)
-  test "play/2" do
-    # If guess is correct:
-    # assert Games.Wordle.play("beats", "beats") == :ok, [:green, :green, :green, :green, :green]
-    assert Games.Wordle.play("beats", "beats") == [:green, :green, :green, :green, :green]
+#   test "All green:" do
+#     assert Games.Wordle.calculate_guess(["a", "a", "a", "a", "a"], ["a", "a", "a", "a", "a"]) == [:green, :green, :green, :green, :green]
+#   end
+#
+#   test "All grey:" do
+#     assert Games.Wordle.calculate_guess(["a", "a", "a", "a", "a"], ["b", "b", "b", "b", "b"]) == [:grey, :grey, :grey, :grey, :grey]
+#   end
+#
+#   test "One green:" do
+#     assert Games.Wordle.calculate_guess(["a", "a", "a", "a", "a"], ["a", "b", "b", "b", "b"]) == [:green, :grey, :grey, :grey, :grey]
+#     assert Games.Wordle.calculate_guess(["a", "b", "b", "b", "b"], ["a", "a", "a", "a", "a"]) == [:green, :grey, :grey, :grey, :grey]
+#   end
+#
+#   test "Parse answer / input:" do
+#     assert Games.Wordle.parse_string("beats") == ["b", "e", "a", "t", "s"]
+#     assert Games.Wordle.parse_string("Beats") == ["b", "e", "a", "t", "s"]
+#     assert Games.Wordle.parse_string("Beats ") == ["b", "e", "a", "t", "s"]
+  # end
+
+  test "Check Green" do
+    assert Games.Wordle.check_green(["a", "a", "a", "a", "a"], ["a", "b", "b", "b", "b"]) == [:green, "b", "b", "b", "b"]
+    assert Games.Wordle.check_green(["a", "b", "b", "b", "b"], ["a", "a", "a", "a", "a"]) == [:green, "a", "a", "a", "a"]
+    assert Games.Wordle.check_green(["a", "b", "b", "b", "b"], ["c", "c", "a", "c", "c"]) == ["c", "c", "a", "c", "c"]
+    assert Games.Wordle.check_green(["a", "a", "a", "a", "a"], ["a", "b", "a", "b", "b"]) == [:green, "b", :green, "b", "b"]
+    assert Games.Wordle.check_green(["a", "a", "a", "a", "a"], ["a", "a", "a", "a", "a"]) == [:green, :green, :green, :green, :green]
+  end
+
+  test "Check Yellow" do
+     assert Games.Wordle.check_yellow(["a", "b", "b", "b", "b"], ["c", "c", "a", "c", "c"]) == ["c", "c", :yellow, "c", "c"]
+     assert Games.Wordle.check_yellow(["a", "b", "b", "b", "b"], ["c", "c", "a", "a", "c"]) == ["c", "c", :yellow, "a", "c"]
+     assert Games.Wordle.check_yellow(["a", "b", "b", "b", "a"], ["c", "c", "a", "a", "c"]) == ["c", "c", :yellow, :yellow, "c"]
+  end
+
+  test "Check Gray" do
+    assert Games.Wordle.check_grey(nil, ["b", "b", "b", "b", "b"]) == [:grey, :grey, :grey, :grey, :grey]
+    assert Games.Wordle.check_grey(nil, [:green, "b", :green, "b", "b"]) == [:green, :grey, :green, :grey, :grey]
+    assert Games.Wordle.check_grey(nil, [:green, "b", :yellow, "b", "b"]) == [:green, :grey, :yellow, :grey, :grey]
   end
 end
